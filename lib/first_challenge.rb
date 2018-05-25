63b07c7c47f4a1e6e9b06bca51af223332e17b7a
Alan Golman <golman.alan@gmail.com>
2018-05-25 21:55:21 +0000

    automatically backed up by learn

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -18,6 +18,7 @@ def first_challenge
   contacts["Freddy Mercury"].each do |attribute, value|
     if attribute == :favorite_icecream_flavors
       value.delete_if do |flavor|
+        flavor == "strawberry"
 
   #remember to return your newly altered contacts hash!
   contacts
